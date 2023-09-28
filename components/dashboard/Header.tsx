6e84403557fa0fdf3182dc9e7c9261bcfa3a399c
GideonOdiokine <gidysmartodiokine@gmail.com>
2023-09-28 01:06:38 +0100

    fix: added search Input

--- components/dashboard/Header.tsx
+++ components/dashboard/Header.tsx
@@ -1,17 +1,22 @@
+import { Grid } from '@mui/material';
 import UserAvatar from '../global/avatar/user-avatar';
 import Search from '../global/search';
 import Heading from './heading';
 
 const Header = () => {
   return (
-    <div className='flex'>
-      <div className="flex gap-4">
+    // <div className="flex">
+    <Grid className="pt-0 flex" item xs={12} container>
+      <Grid className="flex flex-1 gap-4">
         <UserAvatar />
         <Heading title="Welcome back!" desc="Iyene" />
-      </div>
-      <Search />
-    </div>
+      </Grid>
+      <Grid className="hidden lg:block" item xs={6}>
+        <Search placeholder="Search product..." />
+      </Grid>
+    </Grid>
+    // </div>
   );
-}
+};
 
-export default Header
+export default Header;
--- components/global/search/index.tsx
+++ components/global/search/index.tsx
@@ -1,19 +1,19 @@
 
 import Icon from '../Icons';
 
-const Search = () => {
+const Search = ({placeholder}:any) => {
   return (
     <form className="flex items-center border border-[#FE9900] rounded-full relative overflow-hidden w-full p-0 pl-8">
-      <Icon.SearchIcon />
+      <Icon.SearchIcon className="ml-8" />
       <input
         type="search"
         name=""
-        placeholder="Search essential, groceries and many more..."
-        className="w-9/12 ml-6 py-4 bg-transparent focus:border-0 focus:outline-none text-[#969696] text-sm"
+        placeholder={placeholder}
+        className="w-9/12 pl-4 py-4 bg-transparent focus:border-0 focus:outline-none text-[#969696] text-sm"
       />
       <button
         type="submit"
-        className="bg-[#FE9900] border border-[#FE9900] py-4 px-6 ml-5 text-base font-medium"
+        className="bg-[#FE9900] text-white border border-[#FE9900] py-4 px-6 ml-5 text-base font-medium w-[104px]"
       >
         Search
       </button>
