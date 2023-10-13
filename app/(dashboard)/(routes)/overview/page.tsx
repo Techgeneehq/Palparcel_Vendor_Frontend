ad93a5f77e1e6fc5ada9ab0d686e24690d69c9a4
frank john <frankjoe261@gmail.com>
2023-10-13 15:20:42 +0100

    style: changed dashtable component structure

--- app/(dashboard)/(routes)/overview/page.tsx
+++ app/(dashboard)/(routes)/overview/page.tsx
@@ -5,6 +5,147 @@ import Heading from '@/components/dashboard/heading';
 // import SimpleSlider from '@/components/dashboard/simpleSlider';
 import Icon from '@/components/global/Icons';
 import Card from '@/components/global/cards';
+import phone from '@/public/assets/images/phone.png';
+import { GridColDef } from '@mui/x-data-grid';
+import Image from 'next/image';
+
+const columns: GridColDef[] = [
+  { field: 'id', headerName: 'ID', width: 70 },
+  {
+    field: 'productImage',
+    headerName: 'Product Image',
+    width: 130,
+    sortable: false,
+    renderCell: (params) => (
+      <Image src={params.value} alt={`image of ${params.value}`} />
+    ),
+  },
+  { field: 'name', headerName: 'Name', width: 130 },
+  {
+    field: 'price',
+    headerName: 'Price',
+    width: 130,
+    sortable: false,
+  },
+  {
+    field: 'category',
+    headerName: 'Category',
+    sortable: true,
+    width: 160,
+  },
+  {
+    field: 'overallSold',
+    headerName: 'Overall Sold',
+    type: 'number',
+    sortable: false,
+    width: 90,
+  },
+  {
+    field: 'status',
+    headerName: 'Status',
+    sortable: true,
+    width: 130,
+  },
+  {
+    field: 'actions',
+    headerName: '',
+    sortable: false,
+    width: 10,
+  },
+];
+
+const rows = [
+  {
+    id: 1,
+    productImage: phone,
+    name: 'Snow',
+    price: '$1000',
+    category: 'Jon',
+    overallSold: 35,
+    status: 'Jon',
+    actions: ':',
+  },
+  {
+    id: 2,
+    productImage: phone,
+    name: 'Lannister',
+    price: '$1000',
+    category: 'Cersei',
+    overallSold: 42,
+    status: 'Jon',
+    actions: ':',
+  },
+  {
+    id: 3,
+    productImage: phone,
+    name: 'Lannister',
+    price: '$1000',
+    category: 'Jaime',
+    overallSold: 45,
+    status: 'Jon',
+    actions: ':',
+  },
+  {
+    id: 4,
+    productImage: phone,
+    name: 'Stark',
+    price: '$1000',
+    category: 'Arya',
+    overallSold: 16,
+    status: 'Jon',
+    actions: ':',
+  },
+  {
+    id: 5,
+    productImage: phone,
+    name: 'Targaryen',
+    price: '$1000',
+    category: 'Daenerys',
+    overallSold: 22,
+    status: 'Jon',
+    actions: ':',
+  },
+  {
+    id: 6,
+    productImage: phone,
+    name: 'Melisandre',
+    price: '$1000',
+    category: null,
+    overallSold: 150,
+    status: 'Jon',
+    actions: ':',
+  },
+  {
+    id: 7,
+    productImage: phone,
+    name: 'Clifford',
+    price: '$1000',
+    category: 'Ferrara',
+    overallSold: 44,
+    status: 'Jon',
+    actions: ':',
+  },
+  {
+    id: 8,
+    productImage: phone,
+    name: 'Frances',
+    price: '$1000',
+    category: 'Rossini',
+    overallSold: 36,
+    status: 'Jon',
+    actions: ':',
+  },
+  {
+    id: 9,
+    productImage: phone,
+    name: 'Roxie',
+    price: '$1000',
+    category: 'Harvey',
+    overallSold: 65,
+    status: 'Jon',
+    actions: ':',
+  },
+];
 
 const itemData = [
   {title: 'tab 1', index: 0,},
@@ -14,7 +155,7 @@ const itemData = [
 ];
 
 const contentData = [
-  {index: 0, children: <DashTable />},
+  {index: 0, children: <DashTable rows={rows} columns={columns} />},
   {index: 1, children: <Icon.PurchaseIcon />},
   {index: 2, children: <Icon.RefundIcon />},
   {index: 3, children: <Icon.CategoryIcon />},
