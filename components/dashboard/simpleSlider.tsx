7baa78c9f7d8eff38eb1aa162785fe0033e40a15
GideonOdiokine <gidysmartodiokine@gmail.com>
2023-10-13 12:30:45 +0100

    refactor: added icons and image

--- components/dashboard/simpleSlider.tsx
+++ components/dashboard/simpleSlider.tsx
@@ -3,14 +3,14 @@ import { CCarousel, CCarouselItem, CImage } from '@coreui/react';
 
 const SimpleSlider = () => {
   return (
-    <CCarousel className='hidden md:block'  indicators dark>
+    <CCarousel className="hidden md:block mb-[60px]" indicators dark>
       <CCarouselItem>
         <CImage
           className="d-block w-100 object-cover"
           src="/assets/images/banner.png"
           alt="slide 1"
           width={'100%'}
-          height={"100%"}
+          height={'100%'}
         />
       </CCarouselItem>
       <CCarouselItem>
